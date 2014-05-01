604cb381f29decb7216e28a3871136e7c7a8fa6a
Andrew Adare <andrewadare@gmail.com>
2014-05-01 10:47:51 -0400

    added examples/

---
+++ examples/DrawVTX.C
@@ -0,0 +1,32 @@
+#include "SvxTGeo.h"
+
+#include <TCanvas.h>
+#include <TGeoManager.h>
+#include <TGeoVolume.h>
+
+void DrawVTX()
+{
+  SvxTGeo *geo = new SvxTGeo;
+  geo->ReadParFile("parfiles/svxPISA.par");
+
+  // Make an empty 100x100x100 cm^3 space in the experiment hall.
+  geo->MakeTopVolume(200/2, 200/2, 200/2);
+
+  // Place VTX sensors in the volume.
+  geo->AddSensors();
+
+  // Get handles for further manipulation
+  TGeoManager *mgr = geo->GeoManager();
+  TGeoVolume *top = mgr->GetTopVolume();
+
+  // Done building model.
+  // Close geometry to check for problems (overlapping boundaries)
+  mgr->CloseGeometry();
+
+  // Press j,k to zoom; u,i to look up/down; h,l to look left, right.
+  TCanvas *c = new TCanvas("c", "svx model", 1400, 1000);
+  c->SetFillColor(kBlack);
+  top->Draw();
+
+  return;
+}
