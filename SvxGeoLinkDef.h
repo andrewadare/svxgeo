807c103d4d63997e5560b24fde83df4b3a39c3f3
Andrew Adare <adare@rcas2069.rcf.bnl.gov>
2014-05-01 09:49:29 -0400

    initial commit

---
+++ SvxGeoLinkDef.h
@@ -0,0 +1,72 @@
+// $Id:$
+
+#ifdef __CINT__
+
+#pragma link off all globals;
+#pragma link off all classes;
+#pragma link off all functions;
+
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#pragma link C++ class vector<SvxTGeo::GBox>+;
+#pragma link C++ class vector<SvxTGeo::GBox>::*;
+#pragma link C++ class vector<SvxGeoTrack>+;
+#pragma link C++ class vector<SvxGeoTrack>::*;
+
+// #ifdef G__VECTOR_HAS_CLASS_ITERATOR
+// #pragma link C++ operators vector<SvxTGeo::GBox>::iterator;
+// #pragma link C++ operators vector<SvxTGeo::GBox>::const_iterator;
+// #pragma link C++ operators vector<SvxTGeo::GBox>::reverse_iterator;
+// #pragma link C++ operators vector<SvxGeoTrack>::iterator;
+// #pragma link C++ operators vector<SvxGeoTrack>::const_iterator;
+// #pragma link C++ operators vector<SvxGeoTrack>::reverse_iterator;
+// #endif
+
+#pragma link C++ class SvxTGeo+;
+#pragma link C++ struct SvxTGeo::GBox+;
+#pragma link C++ class SvxGeoTrack+;
+#pragma link C++ class SvxGeoHit+;
+#pragma link C++ class SvxProj+;
+
+#endif /* __CINT__ */
+
+/*
+
+#include "SvxProj.h"
+#ifdef __CINT__ 
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#pragma link C++ class vector<SvxTGeo::GBox>+;
+#pragma link C++ class vector<SvxTGeo::GBox>::*;
+#ifdef G__VECTOR_HAS_CLASS_ITERATOR
+#pragma link C++ operators vector<SvxTGeo::GBox>::iterator;
+#pragma link C++ operators vector<SvxTGeo::GBox>::const_iterator;
+#pragma link C++ operators vector<SvxTGeo::GBox>::reverse_iterator;
+#endif
+#endif
+AutoDict_vector_SvxTGeo__GBox_.cxx (END) 
+
+
+
+
+#include "SvxProj.h"
+#ifdef __CINT__ 
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#ifdef G__VECTOR_HAS_CLASS_ITERATOR
+#pragma link C++ operators vector<SvxGeoTrack>::iterator;
+#pragma link C++ operators vector<SvxGeoTrack>::const_iterator;
+#pragma link C++ operators vector<SvxGeoTrack>::reverse_iterator;
+#endif
+#endif
+AutoDict_vector_SvxGeoTrack_.cxx (END) 
+
+
+#include "SvxProj.h"
+#ifdef __CINT__ 
+#pragma link C++ nestedclasses;
+#pragma link C++ nestedtypedefs;
+#endif
+AutoDict_random_access_iterator_SvxGeoTrack_long_.cxx (END) 
+
+*/
---
+++ SvxGeoTrack.h
@@ -0,0 +1,57 @@
+// $Id:$
+
+#ifndef __SVXGEOTRACK_H__
+#define __SVXGEOTRACK_H__
+
+#include <vector>
+#include <Rtypes.h>
+
+class TGeoNode;
+
+class SvxGeoHit
+{
+public:
+  SvxGeoHit();
+  ~SvxGeoHit() {};
+  void Print();
+
+  // This can differ from member x,y,z if geometry is changed after assignment.
+  void GetCurrentXYZ(double &x, double &y, double &z);
+
+  // These variables are "dumb", i.e. not synced to detector geometry.
+  int layer,ladder,sensor,component,tile;
+  double x,y,z;    // Global position.
+  double xs,ys,zs; // Position on sensor wrt center point
+  double ds,dz;    // Residuals to projected track position (s = Rdphi)
+  double xsigma;   // Cluster size/resolution in x ~ s coord (cm)
+  double zsigma;   // Cluster size/resolution in z coord (cm)
+  int status;      // 0 bad, 1+ good.
+  long trkid;      // Use to associate hit to a track
+  float livefrac;  // Fraction of channels in tile that are good
+
+  // This allows querying hit position in current geometry.
+  TGeoNode *node;
+
+  ClassDef(SvxGeoHit,1)
+};
+
+class SvxGeoTrack
+{
+public:
+  SvxGeoTrack();
+  ~SvxGeoTrack() {};
+  SvxGeoHit GetHit(int i);
+  void Print();
+
+  int nhits;
+  int charge;
+  double vx,vy,vz;
+  double mom, phi0, the0;
+  double bfield;
+  std::vector<SvxGeoHit> hits;
+
+  ClassDef(SvxGeoTrack,1)
+};
+
+
+#endif // __SVXGEOTRACK_H__
